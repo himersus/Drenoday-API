28acf85c9a216064b7f61a75be3e458cc8f1f995
Justino Soares <justinocsoares123@gmail.com>
2025-12-22 13:20:10 +0100

    logic deploy

--- src/controller/Project.ts
+++ src/controller/Project.ts
@@ -381,7 +381,7 @@ networks:
             }
         );
 
-        startLogStream(buildDeploy.id, project.domain);
+        //startLogStream(buildDeploy.id, project.domain);
 
         res.status(200).json({ message: "Deploy iniciado" });
     } catch (error: any) {
