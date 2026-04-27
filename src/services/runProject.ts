e375683409fa0d0322b2605be84f614658042a2f
Justino Soares <justinocsoares123@gmail.com>
2026-04-27 08:09:02 +0100

    refactor: melhorar a formatação e a legibilidade do código na função runProject

--- src/services/runProject.ts
+++ src/services/runProject.ts
@@ -1,5 +1,5 @@
 import { exec } from "child_process";
-import prisma  from "../lib/prisma";
+import prisma from "../lib/prisma";
 import { sendSocketContent } from "../sockets";
 import { collectLogs, startLogStream } from "../utils/logs";
 import fs from "fs";
@@ -7,56 +7,68 @@ import path from "path";
 import { getLastCommitFromBranch } from "../utils/github";
 
 type RunProjectResponse = {
-    statusCode: number;
-    message: string;
+  statusCode: number;
+  message: string;
 } | void;
 
-
 const generateEnvContent = (projectEnvs: string[]): string => {
-    let envContent = "";
-    projectEnvs.forEach((envVar: string) => {
-        envContent += `${envVar}\n`;
-    });
-    return envContent;
-}
+  let envContent = "";
+  projectEnvs.forEach((envVar: string) => {
+    envContent += `${envVar}\n`;
+  });
+  return envContent;
+};
 
-export async function runProject(projectId: string, userId: string):  Promise<RunProjectResponse> {
-      const project = await prisma.project.findFirst({
-            where: { id: projectId },
-        });
+export async function runProject(
+  projectId: string,
+  userId: string,
+): Promise<RunProjectResponse> {
+  const project = await prisma.project.findFirst({
+    where: { id: projectId },
+  });
 
-        if (!project) {
-            return { statusCode: 404, message: "Projeto não encontrado" };
-        }
+  if (!project) {
+    return { statusCode: 404, message: "Projeto não encontrado" };
+  }
 
-        const now = new Date();
-        if (!project.date_expire || project.date_expire < now) {
-            return { statusCode: 403, message: "O plano associado a este projeto expirou. Por favor, renove o plano para continuar." };
-        }
+  const now = new Date();
+  if (!project.date_expire || project.date_expire < now) {
+    return {
+      statusCode: 403,
+      message:
+        "O plano associado a este projeto expirou. Por favor, renove o plano para continuar.",
+    };
+  }
 
-        const existUser = await prisma.user.findFirst({
-            where: { id: userId },
-        });
+  const existUser = await prisma.user.findFirst({
+    where: { id: userId },
+  });
 
-        if (!existUser) {
-            return { statusCode: 404, message: "Usuário não encontrado" };
-        }
+  if (!existUser) {
+    return { statusCode: 404, message: "Usuário não encontrado" };
+  }
 
-        if (project.userId !== userId) {
-            return { statusCode: 403, message: "Você não tem permissão para executar este projeto" };
-        }
+  if (project.userId !== userId) {
+    return {
+      statusCode: 403,
+      message: "Você não tem permissão para executar este projeto",
+    };
+  }
 
-        if (!existUser.github_token) {
-            return { statusCode: 400, message: "Token do GitHub não encontrado, tente sincronizar novamente" };
-        }
-        /*if (project.clone !== 'cloned') {
+  if (!existUser.github_token) {
+    return {
+      statusCode: 400,
+      message: "Token do GitHub não encontrado, tente sincronizar novamente",
+    };
+  }
+  /*if (project.clone !== 'cloned') {
             return res.status(400).json({ message: "O repositório ainda não foi clonado completamente" });
         }*/
 
-        const deployDir = process.env.DEPLOY_DIR;
-        const targetPath = `${deployDir}/${existUser.username}/${project.subdomain}`;
+  const deployDir = process.env.DEPLOY_DIR;
+  const targetPath = `${deployDir}/${existUser.username}/${project.subdomain}`;
 
-        const createComposeTreakfik = `
+  const createComposeTreakfik = `
 services:
   ${project.subdomain}:
     build: .
@@ -74,118 +86,126 @@ networks:
   web:
     external: true
 `;
-        // garantir diretório
-        fs.mkdirSync(targetPath, { recursive: true });
+  // garantir diretório
+  fs.mkdirSync(targetPath, { recursive: true });
 
-        // criar docker-compose
-        fs.writeFileSync(
-            path.join(targetPath, "docker-compose.yml"),
-            createComposeTreakfik
-        );
+  // criar docker-compose
+  fs.writeFileSync(
+    path.join(targetPath, "docker-compose.yml"),
+    createComposeTreakfik,
+  );
 
-        const lastCommit = await getLastCommitFromBranch(
-            project.repo_url,
-            project.branch,
-            existUser.github_token!
-        );
+  const lastCommit = await getLastCommitFromBranch(
+    project.repo_url,
+    project.branch,
+    existUser.github_token!,
+  );
 
-        const buildDeploy = await prisma.deploy.create({
-            data: {
-                projectId: projectId,
-                commit_id: lastCommit.sha || "unknown",
-                commit_msg: lastCommit.message || "unknown",
-                commit_author: lastCommit.author || "unknown",
-                commit_email: lastCommit.email || "unknown",
-                commit_date: lastCommit.date || new Date(),
-                commit_branch: project.branch,
-                commit_avatar_url: lastCommit.avatar_url || null,
-            }
+  const buildDeploy = await prisma.deploy.create({
+    data: {
+      projectId: projectId,
+      commit_id: lastCommit.sha || "unknown",
+      commit_msg: lastCommit.message || "unknown",
+      commit_author: lastCommit.author || "unknown",
+      commit_email: lastCommit.email || "unknown",
+      commit_date: lastCommit.date || new Date(),
+      commit_branch: project.branch,
+      commit_avatar_url: lastCommit.avatar_url || null,
+    },
+  });
+  sendSocketContent("deploy_logs", {
+    deployId: buildDeploy.id,
+    projectId: projectId,
+    status: "building",
+    message: "Iniciando build do deploy",
+  });
+  // verificar se existeo dockerfile no targetPath para continuar
+  if (!fs.existsSync(path.join(targetPath, "Dockerfile"))) {
+    await prisma.deploy.update({
+      where: { id: buildDeploy.id },
+      data: {
+        status: "failed",
+        success: false,
+      },
+    });
+
+    sendSocketContent("deploy_logs", {
+      deployId: buildDeploy.id,
+      projectId: projectId,
+      status: "failed",
+      message:
+        "Este projecto não está disponível para deploy, verifique se o Dockerfile existe na raiz do repositório",
+    });
+    return {
+      statusCode: 404,
+      message:
+        "Este projecto não está disponível para deploy, verifique se o Dockerfile existe na raiz do repositório",
+    };
+  }
+
+  // criar a varialeis de ambiente
+  if (project.environments && project.environments.length > 0) {
+    const envContent = generateEnvContent(project.environments);
+    fs.writeFileSync(path.join(targetPath, ".env"), envContent);
+  }
+  // subir container
+  exec(
+    "git pull && docker-compose up -d --build",
+    { cwd: targetPath },
+    async (error, stdout, stderr) => {
+      if (error) {
+        console.error("[docker error]", stderr);
+        await prisma.deploy.update({
+          where: { id: buildDeploy.id },
+          data: {
+            status: "failed",
+            success: false,
+          },
         });
+        const logSplit = stderr.split("\n");
         sendSocketContent("deploy_logs", {
-            deployId: buildDeploy.id,
-            projectId: projectId,
-            status: "building",
-            message: "Iniciando build do deploy"
+          deployId: buildDeploy.id,
+          projectId: projectId,
+          status: "failed",
+          message:
+            logSplit[logSplit.length - 2] ||
+            "Erro desconhecido durante o build do deploy",
         });
-        // verificar se existeo dockerfile no targetPath para continuar
-        if (!fs.existsSync(path.join(targetPath, "Dockerfile"))) {
-            await prisma.deploy.update({
-                where: { id: buildDeploy.id },
-                data: {
-                    status: "failed",
-                    success: false
-                }
-            });
-            sendSocketContent("deploy_logs", {
-                deployId: buildDeploy.id,
-                projectId: projectId,
-                status: "failed",
-                message: "Este projecto não está disponível para deploy, verifique se o Dockerfile existe na raiz do repositório"
-            });
-            return { statusCode: 404, message: "Este projecto não está disponível para deploy, verifique se o Dockerfile existe na raiz do repositório" };
-        }
+        return;
+      }
 
-        // criar a varialeis de ambiente
-        if (project.environments && project.environments.length > 0) {
-            const envContent = generateEnvContent(project.environments);
-            fs.writeFileSync(path.join(targetPath, ".env"), envContent);
-        }
-        // subir container
-        exec(
-            "git pull && docker-compose up -d --build",
-            { cwd: targetPath },
-            async (error, stdout, stderr) => {
-                if (error) {
-                    console.error("[docker error]", stderr);
-                    await prisma.deploy.update({
-                        where: { id: buildDeploy.id },
-                        data: {
-                            status: "failed",
-                            success: false
-                        }
-                    });
-                    const logSplit = stderr.split("\n");
-                    sendSocketContent("deploy_logs", {
-                        deployId: buildDeploy.id,
-                        projectId: projectId,
-                        status: "failed",
-                        message: logSplit[logSplit.length - 2] || "Erro desconhecido durante o build do deploy"
-                    });
-                    return;
-                }
+      console.log("[docker]", stdout);
+      const logSplit = stdout.split("\n");
+      sendSocketContent("deploy_logs", {
+        deployId: buildDeploy.id,
+        projectId: projectId,
+        status: "building",
+        message: logSplit[logSplit.length - 2] || "Build do deploy concluído",
+      });
+      collectLogs(buildDeploy.id, projectId, logSplit);
+      await prisma.deploy.update({
+        where: { id: buildDeploy.id },
+        data: {
+          status: "running",
+          success: true,
+        },
+      });
 
-                console.log("[docker]", stdout);
-                const logSplit = stdout.split("\n");
-                sendSocketContent("deploy_logs", {
-                    deployId: buildDeploy.id,
-                    projectId: projectId,
-                    status: "building",
-                    message: logSplit[logSplit.length - 2] || "Build do deploy concluído"
-                });
-                collectLogs(buildDeploy.id, projectId, logSplit);
-                await prisma.deploy.update({
-                    where: { id: buildDeploy.id },
-                    data: {
-                        status: "running",
-                        success: true
-                    }
-                });
+      await prisma.project.update({
+        where: { id: projectId },
+        data: {
+          run_status: true,
+        },
+      });
 
-                await prisma.project.update({
-                    where: { id: projectId },
-                    data: {
-                        run_status: true,
-                    }
-                });
-                
-                sendSocketContent("deploy_logs", {
-                    deployId: buildDeploy.id,
-                    projectId: projectId,
-                    status: "running",
-                    message: "Deploy executando com sucesso"
-                });
-                startLogStream(buildDeploy.id, projectId, project.subdomain);
-            }
-        );
-        return { statusCode: 200, message: "Deploy iniciado" };
+      sendSocketContent("deploy_logs", {
+        deployId: buildDeploy.id,
+        projectId: projectId,
+        status: "running",
+        message: "Deploy executando com sucesso",
+      });
+      startLogStream(buildDeploy.id, projectId, project.subdomain);
+    },
+  );
+  return { statusCode: 200, message: "Deploy iniciado" };
 }
